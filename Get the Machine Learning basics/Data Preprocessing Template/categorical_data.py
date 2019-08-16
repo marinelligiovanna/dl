26fb7664b24ce1444d260211b0a8458af086b4b0
Giovanna Marinelli <giovanna.marinelli@daemoninvestimentos.com.br>
2019-08-16 19:21:04 -0300

    Data preprocessing

--- Get the Machine Learning basics/Data Preprocessing Template/categorical_data.py
+++ Get the Machine Learning basics/Data Preprocessing Template/categorical_data.py
@@ -2,27 +2,33 @@
 
 # Importing the libraries
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
+
+# Set current folder as working directory
+import os
+
+os.chdir('/home/giovanna/PycharmProjects/DL/Get the Machine Learning basics/Data Preprocessing Template')
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, -1].values
 
 # Taking care of missing data
 from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
 imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
-# Encoding categorical data
-# Encoding the Independent Variable
+# Encoding categorical variables
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
-# Encoding the Dependent Variable
-labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)
+
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+oneHotEncoder = OneHotEncoder(categorical_features=[0])
+X = oneHotEncoder.fit_transform(X).toarray()
+
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
